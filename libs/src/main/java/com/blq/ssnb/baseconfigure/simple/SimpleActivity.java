13a67ce7ee292bfaec6c2608b811354406b463ed
SSNB <Blq_ssnb@outlook.com>
2019-04-09 09:57:25 +0800

    保存下

---
+++ libs/src/main/java/com/blq/ssnb/baseconfigure/simple/SimpleActivity.java
@@ -0,0 +1,16 @@
+package com.blq.ssnb.baseconfigure.simple;
+
+/**
+ * <pre>
+ * ================================================
+ * 作者: BLQ_SSNB
+ * 日期：2019/4/3
+ * 邮箱: blq_ssnb@outlook.com
+ * 修改次数: 1
+ * 描述:
+ *      添加描述
+ * ================================================
+ * </pre>
+ */
+public class SimpleActivity {
+}
